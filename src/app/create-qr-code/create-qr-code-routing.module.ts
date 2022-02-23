450bb31b421dd9f835c2f38655b69f8f3be3c707
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:03:52 +0300

    - Map component 'CreateQrCodeComponent' to a 'path'.

--- src/app/create-qr-code/create-qr-code-routing.module.ts
+++ src/app/create-qr-code/create-qr-code-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateQrCodeComponent } from './create-qr-code.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateQrCodeComponent,
+    children: [],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
