e28f0759ccf3ba707a00eda0f95bfa00e581ac99
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-23 22:02:16 +0300

    - Generate and export component 'FooterComponent'.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FooterComponent]
 })
 export class SharedModule { }
