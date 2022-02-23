ab4816b4627231020df441fc2429f18b70a91a53
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-23 22:45:39 +0300

    - Initialize immutable object 'navItems'.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Immutable from 'immutable';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class NavbarComponent implements OnInit {
+  navItems = Immutable.fromJS([
+    {
+      href: '/create',
+      textContent: 'Create QR Code'
+    },
+    {
+      href: '/create',
+      textContent: 'Scan QR Code'
+    },
+    {
+      href: '/contact-us',
+      textContent: 'Contact Us'
+    },
+    {
+      href: '/about-us',
+      textContent: 'About Us'
+    },
+  ]);
 
   constructor() { }
 
