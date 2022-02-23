5f88619455ffd20df8e8915395ad3a68cebf56e2
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:14:28 +0300

    - Retrieve 'ViewChild' 'BreadcrumbComponent'.

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreadcrumbComponent } from '../shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ContactUsComponent implements OnInit {
+  @ViewChild(BreadcrumbComponent)
+  private _breadcrumbCmp: BreadcrumbComponent | undefined;
 
   constructor() { }
 
