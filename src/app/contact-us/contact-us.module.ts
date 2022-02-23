95484d7e86c9db14ea674bd0575fb6c7c67e1161
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-23 22:03:22 +0300

    - Import module 'SharedModule'.

--- src/app/contact-us/contact-us.module.ts
+++ src/app/contact-us/contact-us.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsComponent } from './contact-us.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { ContactUsComponent } from './contact-us.component';
   ],
   imports: [
     CommonModule,
-    ContactUsRoutingModule
+    ContactUsRoutingModule,
+    SharedModule,
   ]
 })
 export class ContactUsModule { }
