0882261b2a67f250ad407c9102d7dace5507b728
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:00:00 +0300

    - Generate module 'CreateQrCodeModule' with routing support.

---
+++ src/app/create-qr-code/create-qr-code.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateQrCodeRoutingModule } from './create-qr-code-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CreateQrCodeRoutingModule
+  ]
+})
+export class CreateQrCodeModule { }
