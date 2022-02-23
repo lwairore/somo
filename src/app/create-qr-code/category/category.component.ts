52737223327c0149a1080b85e8dc6d342bedd7c7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:54:18 +0300

    - Initialize immutable object 'categories'.

--- src/app/create-qr-code/category/category.component.ts
+++ src/app/create-qr-code/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Immutable from 'immutable';
 
 @Component({
   selector: 'app-category',
@@ -7,6 +8,48 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CategoryComponent implements OnInit {
+  categories = Immutable.fromJS([
+    {
+      textContent: 'URL',
+      featherIcon: 'link',
+      href: '/create/url'
+    },
+    {
+      textContent: 'Text',
+      featherIcon: 'type',
+      href: '/create/text'
+    },
+    {
+      textContent: 'Contact',
+      featherIcon: 'user',
+      href: '/create/contact'
+    },
+    {
+      textContent: 'Email',
+      featherIcon: 'mail',
+      href: '/create/email'
+    },
+    {
+      textContent: 'SMS',
+      featherIcon: 'message-square',
+      href: '/create/sms'
+    },
+    {
+      textContent: 'Geo',
+      featherIcon: 'fe-map-pin',
+      href: '/create/geo'
+    },
+    {
+      textContent: 'Phone',
+      featherIcon: 'phone',
+      href: '/create/phone'
+    },
+    {
+      textContent: 'Wifi',
+      featherIcon: 'wifi',
+      href: '/create/wifi'
+    },
+  ])
 
   constructor() { }
 
