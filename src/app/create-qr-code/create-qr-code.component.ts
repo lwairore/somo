125f4363d758f391ed5202d0bfdc0019960db5fa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:02:24 +0300

    - Generate component 'CreateQrCodeComponent'.

---
+++ src/app/create-qr-code/create-qr-code.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-qr-code',
+  templateUrl: './create-qr-code.component.html',
+  styles: [
+  ]
+})
+export class CreateQrCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/create-qr-code/create-qr-code.module.ts
+++ src/app/create-qr-code/create-qr-code.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreateQrCodeRoutingModule } from './create-qr-code-routing.module';
+import { CreateQrCodeComponent } from './create-qr-code.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateQrCodeComponent],
   imports: [
     CommonModule,
     CreateQrCodeRoutingModule
