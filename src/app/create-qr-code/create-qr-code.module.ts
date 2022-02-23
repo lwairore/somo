e550fb60bfd1db086161e64165b21d2db357f1fe
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:02:53 +0300

    - Format module.

--- src/app/create-qr-code/create-qr-code.module.ts
+++ src/app/create-qr-code/create-qr-code.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CreateQrCodeRoutingModule } from './create-qr-code-routing.module';
 import { CreateQrCodeComponent } from './create-qr-code.component';
 
 
 @NgModule({
-  declarations: [CreateQrCodeComponent],
+  declarations: [
+    CreateQrCodeComponent
+  ],
   imports: [
     CommonModule,
     CreateQrCodeRoutingModule
