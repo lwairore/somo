1b61fd8ac2b8ba7ef48352d5017f00144d62ea71
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:12:50 +0300

    - Initialize immutable object 'breadcrumbItems'.

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Immutable from 'immutable';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BreadcrumbComponent implements OnInit {
+  breadcrumbItems = Immutable.fromJS([]);
 
   constructor() { }
 
