25eb419d4c8140742791f1d712782c0c9d2524ea
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:20:49 +0300

    - Change the 'changeDetection' strategy to 'OnPush'.

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import * as Immutable from 'immutable';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BreadcrumbComponent implements OnInit {
   breadcrumbItems = Immutable.fromJS([]);
