bc5635ee816ef3d5d76faf99733ee16951f7bc6e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 01:02:00 +0300

    - Change the 'changeDetection' strategy to 'OnPush'.

--- src/app/create-qr-code/create-qr-code.component.ts
+++ src/app/create-qr-code/create-qr-code.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
@@ -6,7 +6,8 @@ import { Subscription } from 'rxjs';
   selector: 'app-create-qr-code',
   templateUrl: './create-qr-code.component.html',
   styles: [
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateQrCodeComponent implements OnInit {
   private _routerEventsSubscription: Subscription | undefined;
