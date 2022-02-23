b9be671a927d8405510a928efa11af06f0057d3a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 00:41:59 +0300

    - Handle all unmatched routes.

--- src/app/create-qr-code/create-qr-code-routing.module.ts
+++ src/app/create-qr-code/create-qr-code-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
         path: '',
         component: CategoryComponent,
       },
+      {
+        path: '**',
+        component: CategoryComponent,
+      },
     ],
   },
 ];
