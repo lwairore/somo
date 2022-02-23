9c45f72012d52c69aea3865f5fe6c6ce6cc68366
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-23 20:59:37 +0300

    - Map component 'ContactUsComponent' to a 'path'.

--- src/app/contact-us/contact-us-routing.module.ts
+++ src/app/contact-us/contact-us-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactUsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
