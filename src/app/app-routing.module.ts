0faa2d8955fe0a8e8609e4bc2cbed05a6be28aea
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 21:04:15 +0300

    - Lazy load module 'ScanQrCodeModule'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     loadChildren: () => import('./create-qr-code/create-qr-code.module')
       .then(c => c.CreateQrCodeModule),
   },
+  {
+    path: 'scan',
+    loadChildren: () => import('./scan-qr-code/scan-qr-code.module')
+      .then(s => s.ScanQrCodeModule),
+  },
 ];
 
 @NgModule({
