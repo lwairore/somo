ab42ef2bacefb9048925dbe835d2fc25f50e8202
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 12:40:52 +0300

    - Initialize object 'websiteAddressFormGroup'.

--- src/app/create-qr-code/url/url.component.ts
+++ src/app/create-qr-code/url/url.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-url',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class UrlComponent implements OnInit {
-
+  websiteAddressFormGroup: FormGroup | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
