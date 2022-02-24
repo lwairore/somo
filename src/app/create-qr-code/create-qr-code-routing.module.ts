901a0d9eeb675a2a28c8bf99f3a2f022f7eaa7b4
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 15:57:11 +0300

    - Map component 'TextComponent' to a 'path'.

--- src/app/create-qr-code/create-qr-code-routing.module.ts
+++ src/app/create-qr-code/create-qr-code-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { CreateQrCodeComponent } from './create-qr-code.component';
+import { TextComponent } from './text/text.component';
 import { UrlComponent } from './url/url.component';
 
 
@@ -17,6 +18,10 @@ const routes: Routes = [
       {
         path: 'url',
         component: UrlComponent,
+      },
+      {
+        path: 'text',
+        component: TextComponent,
       },
       {
         path: '**',
