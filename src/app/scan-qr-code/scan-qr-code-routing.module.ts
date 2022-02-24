815a179365a3e217ae29324b3066bafef99ec85c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-24 21:05:48 +0300

    - Map component 'ScanQrCodeComponent' to a path.

--- src/app/scan-qr-code/scan-qr-code-routing.module.ts
+++ src/app/scan-qr-code/scan-qr-code-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScanQrCodeComponent } from './scan-qr-code.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ScanQrCodeComponent,
+    children: [],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
